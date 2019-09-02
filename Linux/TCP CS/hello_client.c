3e7ab54df397976573d15de5fe55eec8b57a5fc1
李瑀辰 <liyuchen1995@live.com>
2019-09-02 14:10:17 +0800

    回声 改进

--- Linux/TCP CS/hello_client.c
+++ Linux/TCP CS/hello_client.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
     int sock;
     struct sockaddr_in serv_addr;
     char message[BUF_SIZE];
-    int str_len;
+    int str_len, recv_len, recv_cnt;
 
     if(argc != 3)
     {
@@ -43,9 +43,20 @@ int main(int argc, char* argv[])
         if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
             break;
 
-        write(sock, message, strlen(message));
-        str_len = read(sock, message, BUF_SIZE-1);
-        message[str_len] = 0;
+        str_len = write(sock, message, strlen(message));
+
+        recv_len = 0;
+
+        // caculate message length, receive whole message
+        while(recv_len < str_len)
+        {
+            recv_cnt = read(sock, message, BUF_SIZE-1);
+            if(recv_cnt == -1)
+                error_handling("read() error");
+            recv_len += recv_cnt;
+        }
+        
+        message[recv_len] = 0;
         printf("Message from server: %s", message);
     }
 
